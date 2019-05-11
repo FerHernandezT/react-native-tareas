e874db562cf7adaf65b4f8490d1cad0f49ee153f
Fehet <fehet@localhost.localdomain>
2019-05-11 12:39:03 -0500

    ADD FILTER BR BY FECHA-ESTADO

--- src/components/TareasList.js
+++ src/components/TareasList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { View, Text, FlatList, RefreshControl } from "react-native";
+import { View, FlatList, RefreshControl } from "react-native";
+import { Icon, Button, Form, Item, Label, Text, Container, Title} from 'native-base';
 import { connect } from 'react-redux';
 
 class TareasList extends Component {
@@ -8,6 +9,8 @@ class TareasList extends Component {
         this.state={
             refreshing:false,
             load: false,
+            fecha: false,
+            estado: false,
         }
     };
 
@@ -31,20 +34,72 @@ class TareasList extends Component {
         //API LOAD DATA
     }
 
+    onFilter(key){
+        this.setState({
+            fecha: (key == 'fecha')? true : false,
+            estado: (key == 'estado')? true : false,
+        })
+    }
+
+    sortData(array){
+        console.log("ESTADO", this.state);
+        array = JSON.parse(JSON.stringify(array));
+        if(!this.state.fecha && !this.state.estado) return array;
+        if(this.state.fecha){
+            console.log("ORDEN-FECHA");
+            array.sort(function (a, b) {
+                
+                if (parseInt(a.id) < parseInt(b.id)) return 1;
+                if (parseInt(a.id) > parseInt(b.id)) return -1;
+                return 0;
+            });
+        }
+        if(this.state.estado){
+            console.log("ORDEN-STADO");
+            array.sort(function (a, b) {
+                if (a.estado > b.estado) return 1;
+                if (a.estado < b.estado) return -1;
+                return 0;
+            });
+        }
+        
+        return array;
+
+    }
+
     render(){
+        let data = this.sortData(this.props.tareas);
+
+
         return(
-            <FlatList
-                data={this.props.tareas}
-                renderItem={this.renderRow.bind(this)}
-                keyExtractor={(item, index) => index.toString()}
-                /*refreshing={true}
-                refreshControl={
-                    <RefreshControl
-                        refreshing={this.state.refreshing}
-                        onRefresh={this._onRefresh.bind(this)}
-                    />
-                }*/
-            />
+            <View style={{marginTop: 5}}>
+                <View style={[styles.row, styles.between]}>
+                    <View style={{flex:1}}>
+                        <Button iconRight full light={(this.state.fecha)? false : true} onPress={()=> this.onFilter('fecha')}>
+                            <Text>Fecha</Text>
+                            <Icon name='md-arrow-dropdown' style={{fontSize:15}} />
+                        </Button>
+                    </View>
+                    <View style={{flex:1}}>
+                        <Button iconRight full light={(this.state.estado)? false : true} onPress={()=> this.onFilter('estado')}>
+                            <Text>Completado</Text>
+                            <Icon name='md-arrow-dropdown' style={{fontSize:15}} />
+                        </Button>
+                    </View>
+                </View>
+                <FlatList
+                    data={data}
+                    renderItem={this.renderRow.bind(this)}
+                    keyExtractor={(item, index) => index.toString()}
+                    /*refreshing={true}
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={this.state.refreshing}
+                            onRefresh={this._onRefresh.bind(this)}
+                        />
+                    }*/
+                />
+            </View>
         )
     }
 }
